e450d787b92504ad8b34763d79de53d67b93ab94
Mathieu Mafille <81818115+mathieumaf@users.noreply.github.com>
2025-10-13 18:56:30 +0200

    Fix favicon paths to respect base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { joinURL } from 'ufo'
+
+const appBaseURL = process.env.NUXT_APP_BASE_URL || '/'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   app: {
+    baseURL: appBaseURL,
     head: {
       title: 'What is my IP? - IP Address Lookup',
       htmlAttrs: {
@@ -16,11 +21,12 @@ export default defineNuxtConfig({
         { name: 'author', content: 'Mathieu Mafille' },
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'icon', type: 'image/x-icon', href: joinURL(appBaseURL, 'favicon.ico') },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: joinURL(appBaseURL, 'favicon.ico') },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: joinURL(appBaseURL, 'favicon-16x16.png') },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: joinURL(appBaseURL, 'favicon-32x32.png') },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: joinURL(appBaseURL, 'apple-touch-icon.png') },
+        { rel: 'manifest', href: joinURL(appBaseURL, 'site.webmanifest') },
       ],
     },
   }
